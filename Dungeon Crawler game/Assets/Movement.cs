c78b79ff5781969cf3b40d3bfc5ec043e74751b7
Braven <bngarnicas@gmail.com>
2019-02-14 10:57:49 -0800

    stuff
    stuff

---
+++ Dungeon Crawler game/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, -5f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 5f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+
+    }
+}
